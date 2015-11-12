0a25210846e00981f285aff4661461fb3a173602
Suresh Nemani <chandra.nemani@gmail.com>
2015-11-12 15:58:38 +0530

    Addted div and sub test code

--- CalGit/src/com/caltest/CalTest.java
+++ CalGit/src/com/caltest/CalTest.java
@@ -1,8 +1,11 @@
 package com.caltest;
 
+import org.junit.After;
+
 //import static org.junit.Assert.*;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.calculator.Calculator;
@@ -11,12 +14,33 @@ import com.calculator.Calculator;
 
 public class CalTest {
 
+	Calculator cal;
+	
+	@Before
+	public void init(){
+		cal = new Calculator();
+	}
+	
+	@After
+	public void cleanup(){
+		cal = null;
+	}
 	@Test
-	public void test() {
+	public void AddTest() {
 
-		Calculator cal = new Calculator();
+		//Calculator cal = new Calculator();
 		Assert.assertTrue(cal.Add(10, 20) == 30);
 				
 	}
+	
+	@Test
+	public void DivTest(){
+		Assert.assertTrue(cal.Div(20, 0)== 2);
+	}
 
+	@Test
+	public void SubTest(){
+		Assert.assertTrue(cal.Sub(20, 10) == 10 );
+	}
+	
 }
